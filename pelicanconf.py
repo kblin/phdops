173833149f1c5451ad203efa7121518073daf016
Kai Blin <kai@samba.org>
2015-04-23 01:17:30 +0200

    Customise the blog a bit
    Signed-off-by: Kai Blin <kai@samba.org>

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,8 @@ from __future__ import unicode_literals
 
 AUTHOR = u'Kai Blin'
 SITENAME = u'PhDOps'
-SITEURL = 'http://phdops.kblin.org'
+SITESUBTITLE = u'Tales from the underfunded cousin of DevOps, while trying to get research done.'
+SITEURL = u'http://phdops.kblin.org'
 
 PATH = 'content'
 
@@ -19,17 +20,21 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
+THEME = u'pelican-bootstrap3'
+
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+#LINKS = (('Pelican', 'http://getpelican.com/'),
+#         ('Python.org', 'http://python.org/'),
+#         ('Jinja2', 'http://jinja.pocoo.org/'),
+#         ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('twitter', 'https://twitter.com/kaiblin'),
+          ('github', 'https://github.com/kblin'),)
+
+TWITTER_USERNAME = u'kaiblin'
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-RELATIVE_URLS = True
+#RELATIVE_URLS = True
